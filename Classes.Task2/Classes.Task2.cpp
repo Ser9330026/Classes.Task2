a2933e2da7abb6b51689f712acb8c55d4517fc83
ser9330026 <ser9330026@yandex.ru>
2023-11-20 20:18:19 +0500

    Добавьте файлы проекта.

---
+++ Classes.Task2/Classes.Task2.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <string>
+
+
+class Counter {
+private:
+    int count;
+
+public:
+    Counter() : count() {};
+    Counter(int c) : count(c) {};
+
+    void increment() {
+        count++;
+    }
+
+    void decrement() {
+        count--;
+    }
+
+    int valFunc() {
+        return count;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "rus");
+        
+    std::cout << "Здание №2" << std::endl;
+ std::string str;
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите yes или no: ";
+    std::cin >> str;
+    int number;
+    std::cout << "Введите начальное значение счетчика: ";
+    std::cin >> number;
+    Counter count(number);
+    char ch;
+    while (str == "yes") {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> ch;
+        if (ch == '+') {
+            count.increment();
+        }
+        if (ch == '-') {
+            count.decrement();
+        }
+        if (ch == '=') {
+            std::cout << count.valFunc() << std::endl;
+        }
+        if (ch == 'x') {
+            break;
+        }
+    }
+    return 0;
+}
+
